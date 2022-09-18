d979c9cc9c7d8889bd0eeb9c7d3ba0850a8ad421
furkan katman <furkan.katman@gmail.com>
2022-09-18 15:10:52 +0300

    -Created and designed and applied some logic.

---
+++ Assesment_furkankatman/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assesment_furkankatman
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            bool? response = openFileDialog.ShowDialog();
+            if (response==true)
+            {
+                string pathToFile = openFileDialog.FileName;
+                MessageBox.Show(pathToFile);
+                using (var reader = new StreamReader(pathToFile))
+                {
+                    List<string> listA = new List<string>();
+                    List<string> listB = new List<string>();
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var values = line.Split(' ');
+
+                        listA.Add(values[0]);
+                        listB.Add(values[1]);
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ Assesment_furkankatman/VolumeCalculator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assesment_furkankatman
+{
+    public class VolumeCalculator:IVolumeCalculator
+    {
+        public List<double> DepthValues  { get; set; }
+        public int BaseHorizon { get; set; }
+        public int FluidContact { get; set; }
+        public int CellSizeWidth { get; set; }
+        public int CellSizeHeight { get; set; }
+        public VolumeCalculator(List<double> depthValues,int baseHorizon,int fluidContact,int cellSizeWidth,int cellSizeHeight)
+        {
+            this.BaseHorizon = baseHorizon;
+            this.CellSizeHeight = cellSizeHeight;
+            this.CellSizeWidth = cellSizeWidth;
+            this.DepthValues = depthValues;
+            this.FluidContact = fluidContact;
+        }
+
+        public int Calculate(List<double> depthValues,int baseHorizon,int fluidContact, int cellSize_Width, int cellSize_Height, UnitEnum unit)
+        {
+            var Volume = 0;
+            foreach (var depth in depthValues)
+            {
+                var baseHorizonActual = depth + baseHorizon;
+                if (fluidContact> depth && fluidContact < baseHorizonActual)
+                {
+                    Volume = (int)(Volume + (cellSize_Height * cellSize_Width * (Math.Abs(depth - fluidContact))));
+                }
+
+                    
+
+            }
+            return Volume;
+            
+        }
+    }
+
+    public interface IVolumeCalculator
+    {
+        public int Calculate(List<double> depthValues, int baseHorizon, int fluidContact, int cellSize_Width, int cellSize_Height,UnitEnum unit);
+
+    }
+    public enum UnitEnum
+    {
+        CubicMeter,
+        Cubicfeet,
+        Barrels
+
+    }
+}
